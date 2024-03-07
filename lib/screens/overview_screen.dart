ba954b44030eba1b6d7bc1760a9ef579fdfa73da
gerablin <schieberdevacc@gmail.com>
2024-03-07 18:41:39 +0100

    add list on overview screen

--- lib/screens/overview_screen.dart
+++ lib/screens/overview_screen.dart
@@ -1,26 +1,102 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:timers/utils/size_config.dart';
+
+import '../models/timer.dart';
+
+WorkoutTimer workoutTimer =
+    WorkoutTimer(workoutCountDown: 3, restCountDown: 2, runs: 3);
+
+List<WorkoutTimer> workouts = [workoutTimer];
 
 class OverviewScreen extends StatelessWidget {
   const OverviewScreen({super.key});
 
-  void _navigateToCreateTimer(BuildContext context) {
+  void _navigateToWorkout(BuildContext context) {
     context.push('/timer/13');
   }
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text("Dein Timer"),
       ),
-      body: const Column(
-        children: [Text("welcome")],
+      body: Column(
+        children: [
+          Text("welcome"),
+          WorkoutList(onClick: () => _navigateToWorkout(context))
+        ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => _navigateToCreateTimer(context),
+        onPressed: () => _navigateToWorkout(context),
         child: Icon(Icons.add),
       ),
+    );
+  }
+}
+
+class WorkoutList extends StatelessWidget {
+  const WorkoutList({super.key, required this.onClick});
+
+  final VoidCallback onClick;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:
+          EdgeInsets.symmetric(horizontal: SizeConfig.blockSizeHorizontal * 4),
+      child: Column(
+        children: [
+          ...workouts.map((workout) => WorkoutCard(
+                workout: workout,
+                onClick: onClick,
+              ))
+        ],
+      ),
+    );
+  }
+}
+
+class WorkoutCard extends StatelessWidget {
+  const WorkoutCard({
+    super.key,
+    required this.workout,
+    required this.onClick,
+  });
+
+  final VoidCallback onClick;
+  final WorkoutTimer workout;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onClick,
+      child: Card(
+          child: Padding(
+        padding: EdgeInsets.symmetric(
+            vertical: SizeConfig.blockSizeVertical,
+            horizontal: SizeConfig.blockSizeHorizontal * 4),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("workout"),
+                Text("${workout.workoutCountDown} seconds")
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text("rest"),
+                Text("${workout.restCountDown} seconds")
+              ],
+            )
+          ],
+        ),
+      )),
     );
   }
 }
